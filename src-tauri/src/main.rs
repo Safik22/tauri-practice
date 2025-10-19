42e3445cc95d830bfbfa50f30dc17079b915ef6e
dana <danask2006@gmail.com>
2025-10-19 17:33:53 +0700

    add basic audio handling structure in Rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod audio;
 
 fn main() {
-    test_app_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            audio::get_audio_state_command,
+            audio::set_volume_command,
+            audio::set_mute_command,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri app");
 }
